1e24a8f44f105f065ca05ae9065566b4fbbf98b0
eun <chocoding0@naver.com>
2021-12-29 22:09:23 +0900

    ..

---
+++ Algorithm_study/Algorithm_2.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+
+//int main()
+//{
+	//점수 구하기
+	/*int a; 
+	scanf("%d", &a); 
+	printf("%c", "FFFFFFDCBAA"[a / 10]);*/
+
+	//윤년 구하기
+	/*int a;
+	
+	scanf("%d", &a);
+
+	if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+		printf("1\n");
+	}
+	else {
+		printf("0\n");
+	}*/
+
+	//사분면 고르기
+	/*int x, y;
+	scanf("%d %d", &x, &y);
+
+	if (x > 0 && y > 0) {
+		printf("1");
+	}
+	else if (x < 0 && y > 0) {
+		printf("2");
+	}
+	else if (x < 0 && y < 0) {
+		printf("3");
+	}
+	else if (x > 0 && y < 0) {
+		printf("4");
+	}
+	else {
+		;
+	}*/	//방법1
+	//printf("%d", x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3);	//방법2
+	//printf("%d", 1 + (y < 0) * 2 + (x * y < 0));	//방법3
+
+	//int h, m;
+
+//}
