213880a89921e9b1b78f8505146db86dcfd10acb
eun <chocoding0@naver.com>
2021-12-29 22:57:12 +0900

    for문 활용

--- Algorithm_study/기본 연습/using_for.cpp
+++ Algorithm_study/기본 연습/using_for.cpp
@@ -1,13 +1,13 @@
 #include <cstdio>
 
-//int main()
-//{
-	//점수 구하기
+int main()
+{
+	//<B_9498> 점수 구하기
 	/*int a; 
 	scanf("%d", &a); 
 	printf("%c", "FFFFFFDCBAA"[a / 10]);*/
 
-	//윤년 구하기
+	//<B_2753> 윤년 구하기
 	/*int a;
 	
 	scanf("%d", &a);
@@ -19,7 +19,7 @@
 		printf("0\n");
 	}*/
 
-	//사분면 고르기
+	//<B_14681> 사분면 고르기
 	/*int x, y;
 	scanf("%d %d", &x, &y);
 
@@ -41,6 +41,22 @@
 	//printf("%d", x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3);	//방법2
 	//printf("%d", 1 + (y < 0) * 2 + (x * y < 0));	//방법3
 
-	//int h, m;
+	//<B_2664> 알람시계
+	int h, m, res_h, res_m;
 
-//}
+	scanf("%d%d", &h, &m);
+
+	res_h = h;
+	res_m = m - 45;
+
+	if (res_m < 0) {
+		res_m += 60;
+		res_h -= 1;
+	}
+	if (res_h < 0) {
+		res_h += 24;
+	}
+
+	printf("%d %d\n", res_h, res_m);
+	printf("%d %d\n", (h + 24 - (m < 45)) % 24, (m + 15) % 60);	//방법2
+}
